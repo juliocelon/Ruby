e15257c0553264e89b674d16deb290e0a369fa16
Julio César Celón Altamirano <37186598+juliocelon@users.noreply.github.com>
2022-05-12 11:18:15 -0500

    Specify the value that is not inherited
    Specify the value that is not inherited

--- class_or_instance_variables.rb
+++ class_or_instance_variables.rb
@@ -1,21 +1,25 @@
+# Variables of class and variables of instance.
 class Video
-  @@class_variable = "My variable of class"
-  @instance_variable = "My variable of instance"
+    @@class_variable = "My variable of class"
+    @instance_variable = "My variable of instance"
 
-  def self.test
-    p @@class_variable
-    p @instance_variable
+    def self.test
+      p @@class_variable
+      p @instance_variable
+    end
   end
-end
-
-class YouTube < Video
-  def self.test
-    @@class_variable = "My variable of class has changed"
-    p @@class_variable # The value of the variable of class is inherited
-    p @instance_variable # The value of the variable of instance is NOT inherited
+  
+  class YouTube < Video
+    def self.test
+      @@class_variable = "My variable of class has changed"
+      p @@class_variable       # The value of the variable of class is inherited
+      p @instance_variable     # The VALUE of the variable of instance is NOT inherited
+      @instance_variable = "My variable of INSTANCE has changed"
+      p @instance_variable     
+    end
   end
-end
-
-YouTube.test
-Video.test
-
+  
+  puts "Calling YouTube.test:"
+  YouTube.test
+  puts "Calling Video.test:"
+  Video.test
